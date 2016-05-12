d5c58340b1b3a450393a3f0b659be5efe47835b9
jennyChing <jklife3@gmail.com>
2016-05-13 00:11:51 +0800

    tree diameter

--- 10308.py
+++ 10308.py
@@ -9,7 +9,6 @@ The input contains several sets of input. Each set of input is a sequence of lin
 Output
 For each set of input, you are to output a single line containing a single integer: the road distance between the two most remote villages in the area.
 '''
-dia = 0
 v_list = []
 v_len = 0
 
@@ -20,37 +19,39 @@ def adj(v_list):
         v_adj[v[1] - 1][v[0] - 1] = v[2]
     return v_adj
 
-def DFS(root, v_adj):
+def DFS(x, px, v_adj, dia): # px is the father of p
+    print("dia", dia)
     h1, h2 = 0, 0 # record the first and second height
     for y in range(len(v_adj)):
-        if v_adj[x][y] != 0:
-            h = DFS(y, x, v_list) + 1
+        if v_adj[x][y] != 0 and y != px:
+            h = DFS(y, x, v_adj, dia)[0] + v_adj[y][x]
+            print(h, h1, h2)
             if h > h1:
                 h2, h1 = h1, h
             elif h > h2:
-                h2 = h
-    dia = max(dia, h1 + h2) # pass through root's first and second longest
-    return h1
+                h = h
+    dia = max(dia, h1 + h2)
+    print(dia)
+    return h1, dia
 
 
 def diameter(v_list):
+    dia = 0
     v_adj = adj(v_list) # turn the adjacency list into matrix
-    root = 0
-    return DFS(root, root, v_adj)
-
-
+    print(v_adj)
+    root = 3  # every node in a tree can be the root
+    return DFS(root, root, v_adj, dia)
 
 if __name__ == '__main__':
     while True:
         try:
             v = list(map(int, input().split()))
             if len(v) == 0:
-                print(diameter(v_list))
+                print(diameter(v_list)[1])
                 v_list = []
             else:
-                print(v)
                 v_len = max(v[0], v[1], v_len)
                 v_list.append(v)
         except(EOFError):
-            print(diameter(v_list))
+            print(diameter(v_list)[1])
             break
