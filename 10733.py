cd161276b5f8bb7c2c9567d76a45bc8f4eeaaac0
jennyChing <jklife3@gmail.com>
2016-05-26 23:51:40 +0800

    permutations

---
+++ 10733.py
@@ -0,0 +1,9 @@
+'''
+Permutations
+
+10733 - The Colored Cubes
+
+All 6 sides of a cube are to be coated with paint. Each side is is coated uniformly with one color. When a selection of n different colors of paint is available, how many different cubes can you make?  Note that any two cubes are only to be called “different” if it is not possible to rotate the one into such a position that it appears with the same coloring as the other.
+'''
+if __name__ =='__main__':
+
