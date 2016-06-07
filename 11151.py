04c60db0656615ce0694f43240534eabd67bbd28
jennyChing <jklife3@gmail.com>
2016-06-07 23:42:17 +0800

    Palindrome

---
+++ 11151.py
@@ -0,0 +1,15 @@
+'''
+
+Palindrome
+
+11151 - Longest Palindrome
+
+For each input string, your program should print the length of the longest palindrome you can get by removing zero or more characters from it.
+'''
+if __name__ == '__main__':
+    cases = int(input())
+    for _ in range(cases):
+        content = input()
+        for i in range(len(content)):
+
+
