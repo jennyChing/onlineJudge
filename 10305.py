3090c1677fd1b8bdb3af5bd24123aaac9c7ce290
jennyChing <jklife3@gmail.com>
2016-06-21 23:37:30 +0800

    directed acycle graph

--- 10305.py
+++ 10305.py
@@ -1,7 +1,7 @@
 '''
-Directed Acyclic Graph （ DAG ）
-
 Topological Ordering
+
+10305 - Ordering Tasks
 
 Input
 The input will consist of several instances of the problem. Each instance begins with a line containing two integers, 1 ≤ n ≤ 100 and m. n is the number of tasks (numbered from 1 to n) and m is the number of direct precedence relations between tasks. After this, there will be m lines with two integers i and j, representing the fact that task i must be executed before task j.  An instance with n = m = 0 will finish the input.
@@ -18,4 +18,23 @@ if __name__ == '__main__':
         for _ in range(m):
             i, j = list(map(int, input().split()))
             order[j] = i
+        trav = set()
+        task_order = []
+        for i in range(1, n + 1):
+            if i not in order and i not in trav:
+                trav.add(i)
+                task_order.append(i)
+            elif i in trav:
+                pass
+            else:
+                while True:
+                    if order[i]
+                    if order[i] not in order and order[i] not in trav:
+                        trav.add(order[i])
+                        task_order.append(order[i])
+                        break
+                    else:
+                        print(i, order[i])
+                        i = order[i]
+        print(order, task_order)
 
