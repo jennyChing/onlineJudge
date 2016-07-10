0b7b23462b97bf1d832668862df4c6cbba6ed5b1
jennyChing <jklife3@gmail.com>
2016-07-10 23:38:06 +0800

    Rectangles

--- 10173.py
+++ 10173.py
@@ -9,3 +9,20 @@ The input file may contain multiple test cases. Each test case begins with a lin
 Output
 For each test case in the input print a line containing the area of the smallest bounding rectangle rounded to the 4th digit after the decimal point.
 '''
+if __name__ == '__main__':
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        x = []
+        y = []
+        for _ in range(n):
+            point = list(map(float, input().split()))
+            x.append(point[0])
+            y.append(point[1])
+        max_x = max(x)
+        max_y = max(y)
+        min_x = min(x)
+        min_y = min(y)
+        print("{0:0.4f}".format((max_x - min_x) * (max_y - min_y)))
+
